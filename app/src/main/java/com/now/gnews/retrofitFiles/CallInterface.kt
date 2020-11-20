ab7d7f4c27fda2d391dc307dc7dc36cbcb7ee499
Lesiba-SA <64387134+Lesiba-SA@users.noreply.github.com>
2020-11-21 01:57:52 +0200

    Models and Retrofit classes done

---
+++ app/src/main/java/com/now/gnews/retrofitFiles/CallInterface.kt
@@ -0,0 +1,31 @@
+package com.now.gnews.retrofitFiles
+
+import com.now.gnews.models.ApiResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CallInterface {
+
+    @GET("v2/everything")
+    suspend fun getAllNews(
+        @Query("q")
+        country: String = "us",
+        @Query("page")
+        page: Int = 1,
+        @Query("apikey")
+        apikey: String = ""
+    ) : Response<ApiResponse>
+
+    @GET("v2/everything")
+    suspend fun getNewsFromCategory(
+        @Query("q")
+        country: String = "us",
+        @Query("page")
+        page: Int = 1,
+        @Query("category")
+        category: String,
+        @Query("apikey")
+        apikey: String = ""
+    ) : Response<ApiResponse>
+}
